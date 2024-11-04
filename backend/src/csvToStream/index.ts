92c9afb6afd5bb1b189f71a0d3da7453cfa4d1ed
Lauri Telama <lauri.telama@gmail.com>
2024-11-04 22:02:32 +0200

    Send raw trade events in batches of 10k messages and log progress of processing the file

--- backend/src/csvToStream/index.ts
+++ backend/src/csvToStream/index.ts
@@ -14,8 +14,13 @@ export type ParsedRawData = {
   tradingTime?: google.protobuf.Timestamp
 }
 
+const LOG_EVERY_X_LINES_PROCESSED = 100_000
+const PRODUCE_DATA_BATCH_SIZE = 10_000
+
 export const main = async () => {
   await producer.connect()
+
+  let lineCounter = 0
 
   try {
     // Find the csv files
@@ -34,6 +39,9 @@ export const main = async () => {
       })
 
       let headers
+
+      let datapoints: Array<ParsedRawData> = []
+
       for await (const line of readLineInterface) {
         // Ignore comments
         if (line.startsWith('#')) continue
@@ -46,9 +54,22 @@ export const main = async () => {
 
         // Parse each row of trade data
         const data = parseTradeData(line, headers)
+        if (data) {
+          datapoints.push(data)
+        }
 
-        // Produce raw trade data
-        if (data) await produceRawTradeData(data)
+        // Produce raw trade data points
+        if (datapoints.length % PRODUCE_DATA_BATCH_SIZE === 0) {
+          await produceRawTradeData(datapoints)
+          datapoints = []
+        }
+
+        lineCounter++
+
+        if (lineCounter % LOG_EVERY_X_LINES_PROCESSED === 0) {
+          console.log(new Date().toISOString())
+          console.log(`Total lines processed so far: ${lineCounter}`)
+        }
       }
     }
     console.log('All messages produced successfully')
