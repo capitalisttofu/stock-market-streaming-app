265f9ef1b645e27d5bff1b1d09b0cee815df1742
Lauri Telama <lauri.telama@gmail.com>
2024-11-05 00:15:50 +0200

    Add more performant handling of trade event and discarded data in rawTradeEventParser

--- backend/src/rawTradeEventParser/produceTradeData.ts
+++ backend/src/rawTradeEventParser/produceTradeData.ts
@@ -3,14 +3,12 @@ import { TradeEvent } from '../../generatedProto/compiled'
 import { TRADE_DATA_TOPIC } from '../constants'
 import { producer } from '../lib/kafka'
 
-export const produceTradeData = async (datapoint: ParsedTradeEvent) => {
+export const produceTradeData = (datapoint: ParsedTradeEvent) => {
   try {
     const encoded = TradeEvent.encode(datapoint).finish()
     const buffer = Buffer.from(encoded)
 
-    console.log("Trade data produced")
-
-    await producer.send({
+    producer.send({
       topic: TRADE_DATA_TOPIC,
       // All messages with same symbol go to the same partition
       messages: [{ value: buffer, key: datapoint.symbol }],
