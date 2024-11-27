ac07a09c35c221ab862fe7c38ae0774e41459890
Anton <anton.chakhovich@gmail.com>
2024-11-27 21:35:26 +0200

    Add new env library and move event time difference waiting to csv parser

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,4 +1,6 @@
-import * as env from 'env-var'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 // For now assume we are running this locally
 const HOST_IP = 'localhost'
@@ -10,24 +12,9 @@ export const BUY_SELL_ADVICE_TOPIC = 'buy_sell_advice'
 export const DISCARDED_DATA_TOPIC = 'discarded_data'
 export const EMA_RESULTS_TOPIC = 'ema_results'
 
-export const RECREATE_RAW_TRADE_DATA_TOPIC_ON_PROVISION = env
-  .get('RECREATE_RAW_TRADE_DATA_TOPIC_ON_PROVISION')
-  .default('false')
-  .asBool()
+export const RECREATE_RAW_TRADE_DATA_TOPIC_ON_PROVISION =
+  process.env.RECREATE_RAW_TRADE_DATA_TOPIC_ON_PROVISION === 'true'
 
-export const WAIT_TIME_DIFFERENCE_BETWEEN_EVENTS = env
-  .get('WAIT_TIME_DIFFERENCE_BETWEEN_EVENTS')
-  .default('false')
-  .asBool()
-
-export const REALTIME_DATA_PRODUCTION_START_HOUR = env
-  .get('REALTIME_DATA_PRODUCTION_START_HOUR')
-  .default(0)
-  .asInt()
-
-export const REALTIME_DATA_PRODUCTION_END_HOUR = env
-  .get('REALTIME_DATA_PRODUCTION_END_HOUR')
-  .default(24)
-  .asInt()
+export const NOW_DATE = process.env.NOW_DATE
 
 export const rawDataDirectory = 'rawData'
